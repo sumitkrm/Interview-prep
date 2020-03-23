ccf366e7932d48fe0a2b2c0d3a619c07aa9e6135
Sumit kumar mishra <sumitkrmishra24@gmail.com>
2020-03-24 02:31:59 +0530

    Update 01-pill-bottles.py

--- CTCI/CH_6/01-pill-bottles.py
+++ CTCI/CH_6/01-pill-bottles.py
@@ -1,6 +1,30 @@
+def pill_bottle(bottles):
+    pills = []
+    for i, bottle in enumerate(bottles):
+        pills.append(bottle.pill() * i)
+    weight = use_scale(pills)
+    index = (weight - 190) * 10
+    return int(index + 0.1)
+	
+def use_scale(pills):
+    return sum(pills)
+  
+class Bottle():
+    def __init__(self, pill_weight = 1.0):
+        self.pill_weight = pill_weight
+     
+    def pill(self):
+        return self.pill_weight
+
 import unittest
 
-class test(unittest.Testcase):
+class test(unittest.TestCase):
+    def test_pill_bottle(self):
+        bottles = [Bottle(1.1), Bottle(), Bottle(), Bottle(), Bottle(),
+                    Bottle(), Bottle(), Bottle(), Bottle(), Bottle(),
+                    Bottle(), Bottle(), Bottle(), Bottle(), Bottle(),
+                    Bottle(), Bottle(), Bottle(), Bottle(), Bottle(),]
+        self.assertEqual(pill_bottle(bottles), 0)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
    unittest.main()
